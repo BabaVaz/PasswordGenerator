fc9b37213a2c3a815a0ecdbdb55e45a3d887252f
BabaVaz <v.babakhanian@gmail.com>
2025-07-16 17:24:30 +0200

    Basic Setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let PASS1=document.getElementById("Pass-1")
+let PASS2=document.getElementById("Pass-2")
 
-
-
+function PassGenerator(){
+    PASS1.textContent="Hello"
+}
