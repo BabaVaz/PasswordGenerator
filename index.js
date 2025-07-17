edb9dcbc65eb89d8fb92a91dd274d098e0ae92d5
BabaVaz <v.babakhanian@gmail.com>
2025-07-17 18:51:32 +0200

    Set password length

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+const Symbols= ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+
 let password1= ""
+let password2= ""
 let PASS1=document.getElementById("Pass-1")
 let PASS2=document.getElementById("Pass-2")
+let Passshort=false
+let Sym=false
+let Num=false
+let Char=false
+
+function shortPASS(){
+    Passshort=!Passshort;
+    console.log("The new value is: ", Passshort);
+}
+
+function Symb(){
+    Sym=!Sym;
+    console.log("The new value is: ", Sym);
+}
+
+function Numb(){
+    Num=!Num;
+    console.log("The new value is: ", Num);
+}
 
 function Generator(){
     return Math.floor(Math.random()*characters.length)
@@ -10,19 +35,45 @@ function Generator(){
 
 
 function PassGenerator(){
-    PASS1.textContent=""
-    password1=""
-    for (i=0; i<16; i++){
-    let randomCharacter=characters[Generator()]
-    PASS1.textContent+=randomCharacter
-    password1+=randomCharacter
+PASS1.textContent=""
+password1=""
+let allowedcharacter= [Symbols + characters + Numbers]
+if (Sym===true){
+    allowedcharacter=[characters+Numbers]}
+    else if (Num===true){
+    allowedcharacter=[characters+Symbols]}
+    else if (Sym===true && Num===true){
+    allowedcharacter=[characters]    
+    }
+
+
+    if (Passshort===false){
+        for (i=0; i<16; i++){
+        let randomCharacter=characters[Generator()]
+        PASS1.textContent+=randomCharacter
+        password1+=randomCharacter}}
+    else if (Passshort===true){
+        for (i=0; i<9; i++){
+        let randomCharacter=characters[Generator()]
+        PASS1.textContent+=randomCharacter
+        password1+=randomCharacter}}
+
+PASS2.textContent=""
+password2=""
+    if (Passshort===false){
+        for (i=0; i<16; i++){
+        let randomCharacter=characters[Generator()]
+        PASS2.textContent+=randomCharacter
+        password2+=randomCharacter}}
+    else if (Passshort===true){
+        for (i=0; i<9; i++){
+        let randomCharacter=characters[Generator()]
+        PASS2.textContent+=randomCharacter
+        password2+=randomCharacter}}
 }
-    console.log(password1)
-    PASS2.textContent=""
-    for (i=0; i<16; i++){
-    PASS2.textContent+=characters[Generator()]}
-}
+
 function copyPassword(){
     navigator.clipboard.writeText(PASS1.textContent)
     alert("Copied to Clipboard")
 }
+
